ab9dba72fdaf0843e5f7c7db036ede456571ee64
WISDOM <onuheuniceoyiefie@gmail.com>
2026-06-25 13:15:48 +0000

    Issue #589: Add snapshot testing for demo UI components, CI workflow and docs

---
+++ demo/jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
+};
